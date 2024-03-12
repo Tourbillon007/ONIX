cfd04c85196b313be70c3b34b3f604e0de75428d
Tourbillon007 <1225190818@qq.com>
2024-03-12 22:44:53 +0800

    printk的实现

--- onix/src/kernel/main.c
+++ onix/src/kernel/main.c
@@ -4,39 +4,24 @@
 #include <onix/string.h>
 #include <onix/console.h>
 #include <onix/stdarg.h>
-
-// char message1[] = "hello onix\n";
-// char message2[] = "hello \n";
-// char buf[1024];
+#include <onix/printk.h>
 
 
-void test_args(int cnt, ...)
-{
-    va_list args;
-    va_start(args, cnt);
-
-    int arg;
-    while (cnt--)
-    {
-        arg = va_arg(args, int);
-    }
-    va_end(args);
-}
 
 
 void kernel_init()
 {
  
     console_init();
-    test_args(5, 1, 0xaa, 5, 0x55, 10);
+   
+   int cnt = 30;
+   while (cnt--)
+   {
+        printk("hello onix %#010x\n", cnt);
+   }
+   
 
 
-    // u32 count = 32;
-    // while (count--)
-    // {
-    //     console_write(message1,sizeof(message1) - 1);
-    //     console_write(message2,sizeof(message2) - 1);
-    // }
 
     return 0;
 }
